f786d55618b818d5afce260f490a300625da6989
grayduf <143116744+grayduf@users.noreply.github.com>
2024-02-14 15:16:33 -0500

    update color

--- setting.py
+++ setting.py
@@ -5,11 +5,11 @@ class Setting:
     fps = 30
     window = None
 
-    cell_size = 10
+    cell_size = 5
     cols = height//cell_size
     rows = width//cell_size
     offset = 0
 
-    Colors  = [(0, 0, 0), (255, 192, 0)]
+    Colors  = [(0, 0, 0), (161, 0, 219)]
     black = (0, 0, 0)
     white = (255, 255, 255)
